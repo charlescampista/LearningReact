bee4bf98ef65be8e85c34fd9c08d97402058b547
Charles Campista <charlescampista@gmail.com>
2019-06-18 13:01:11 -0300

    React Hooks - Functional Components

--- Projects/exampleapp/src/App.js
+++ Projects/exampleapp/src/App.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+   const [personState, setPeopleState] = useState({
     person: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
-    ]
-  }
+    ],
+    otherState: 'some other value'
+  });
 
-  swhitchNameHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState('some other value');
+  console.log(personState, otherState);
+  
+
+  const swhitchNameHandler = () => {
+    setPeopleState({  
       person: [
         {name: 'Maximilian', age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27}
-      ]
+      ],
+      otherState: personState.otherState
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-        <p>THIS IS REALLY WORKING!</p>
-        <button onClick={this.swhitchNameHandler}>Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}>Argumento entre tags</Person>
-      </div>
-    ); 
-  }
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>THIS IS REALLY WORKING!</p>
+      <button onClick={swhitchNameHandler}>Switch Name</button>
+      <Person name={personState.person[0].name} age={personState.person[0].age}/>
+      <Person name={personState.person[1].name} age={personState.person[1].age}/>
+      <Person name={personState.person[2].name} age={personState.person[2].age}>Argumento entre tags</Person>
+    </div>
+  ); 
+  
 }
 
-export default App;
+export default app;
