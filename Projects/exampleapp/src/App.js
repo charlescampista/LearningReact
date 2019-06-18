ea4d5e34100be501c7a87f07aa8eac80fcb73a5e
Charles Campista <charlescampista@gmail.com>
2019-06-18 11:32:43 -0300

    Adding project

---
+++ Projects/exampleapp/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    person: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+  swhitchNameHandler = () => {
+    this.setState({
+      person: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <p>THIS IS REALLY WORKING!</p>
+        <button onClick={this.swhitchNameHandler}>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}>Argumento entre tags</Person>
+      </div>
+    ); 
+  }
+}
+
+export default App;
